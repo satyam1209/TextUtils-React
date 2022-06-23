2c14639f5a5576c98f4cebf4dc4b4c72eb9f17cf
satyam1209 <satyamsingh12092000@gmail.com>
2022-06-23 09:06:03 +0530

    first commit

---
+++ precache-manifest.108b1ac9a86ee507c5d7dfd5dad899b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a8dee317358ed27e865",
+    "url": "/TextUtils-React/static/css/main.d987ce55.chunk.css"
+  },
+  {
+    "revision": "9a8dee317358ed27e865",
+    "url": "/TextUtils-React/static/js/main.9a8dee31.chunk.js"
+  },
+  {
+    "revision": "8f2f4211b0891106f089",
+    "url": "/TextUtils-React/static/js/1.8f2f4211.chunk.js"
+  },
+  {
+    "revision": "67a84f94a3104a39a4e6",
+    "url": "/TextUtils-React/static/js/2.67a84f94.chunk.js"
+  },
+  {
+    "revision": "dd3fe261a116bb9c0f41",
+    "url": "/TextUtils-React/static/js/runtime~main.dd3fe261.js"
+  },
+  {
+    "revision": "0c1503e02885a1fc4d1b65fd32de7a55",
+    "url": "/TextUtils-React/index.html"
+  }
+];
